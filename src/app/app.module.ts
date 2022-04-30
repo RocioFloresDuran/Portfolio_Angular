6bea175c9c4414d7b6bc09c96749f930f20e8bca
RocioFloresDuran <rochy.dmf@gmail.com>
2022-04-30 19:32:21 -0300

    Migración del Portfolio a Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    ProyectosComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule
